28b2226d02cf5dfbf6557cedaee8d8d03055ac37
sharvali-shinde-TA <sharvali.shinde@tigeranalytics.com>
2023-07-27 17:08:35 +0530

    production scripts

--- production/feature_engineering.py
+++ production/feature_engineering.py
@@ -8,22 +8,20 @@ training/scoring pipelines.
 """
 import logging
 import os.path as op
-
 from category_encoders import TargetEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 
 from ta_lib.core.api import (
+    DEFAULT_ARTIFACTS_PATH,
     get_dataframe,
     get_feature_names_from_column_transformer,
     get_package_path,
     load_dataset,
     register_processor,
     save_pipeline,
-    DEFAULT_ARTIFACTS_PATH
 )
-
 from ta_lib.data_processing.api import Outlier
 
 logger = logging.getLogger(__name__)
@@ -33,8 +31,8 @@ logger = logging.getLogger(__name__)
 def transform_features(context, params):
     """Transform dataset to create training datasets."""
 
-    input_features_ds = "train/sales/features"
-    input_target_ds = "train/sales/target"
+    input_features_ds = "train/features"
+    input_target_ds = "train/target"
 
     artifacts_folder = DEFAULT_ARTIFACTS_PATH
 
@@ -47,9 +45,7 @@ def transform_features(context, params):
 
     # Treating Outliers
     outlier_transformer = Outlier(method=params["outliers"]["method"])
-    train_X = outlier_transformer.fit_transform(
-        train_X, drop=params["outliers"]["drop"]
-    )
+    train_X = outlier_transformer.fit_transform(train_X, drop=params["outliers"]["drop"])
 
     # NOTE: You can use ``Pipeline`` to compose a collection of transformers
     # into a single transformer. In this case, we are composing a
@@ -58,7 +54,6 @@ def transform_features(context, params):
     # values using ``most_frequent`` strategy.
     tgt_enc_simple_impt = Pipeline(
         [
-            ("target_encoding", TargetEncoder(return_df=False)),
             ("simple_impute", SimpleImputer(strategy="most_frequent")),
         ]
     )
@@ -68,20 +63,6 @@ def transform_features(context, params):
     # for sequential transforms use a pipeline as shown above.
     features_transformer = ColumnTransformer(
         [
-            # categorical columns
-            (
-                "tgt_enc",
-                TargetEncoder(return_df=False),
-                list(
-                    set(cat_columns)
-                    - set(["technology", "functional_status", "platforms"])
-                ),
-            ),
-            (
-                "tgt_enc_sim_impt",
-                tgt_enc_simple_impt,
-                ["technology", "functional_status", "platforms"],
-            ),
             # numeric columns
             ("med_enc", SimpleImputer(strategy="median"), num_columns),
         ]
@@ -100,12 +81,11 @@ def transform_features(context, params):
         sample_X = train_X
     sample_y = train_y.loc[sample_X.index]
 
-
     # Train the feature engg. pipeline prepared earlier. Note that the pipeline is
     # fitted on only the **training data** and not the full dataset.
     # This avoids leaking information about the test dataset when training the model.
     # In the below code train_X, train_y in the fit_transform can be replaced with
-    # sample_X and sample_y if required. 
+    # sample_X and sample_y if required.
     train_X = get_dataframe(
         features_transformer.fit_transform(train_X, train_y),
         get_feature_names_from_column_transformer(features_transformer),
@@ -115,32 +95,8 @@ def transform_features(context, params):
     # a specified set of columns. But, we don't do that here to illustrate
     # what to do when transformations don't cleanly fall into the sklearn
     # pattern.
-    curated_columns = list(
-        set(train_X.columns.to_list())
-        - set(
-            [
-                "manufacturer",
-                "inventory_id",
-                "ext_grade",
-                "source_channel",
-                "tgt_enc_iter_impt_platforms",
-                "ext_model_family",
-                "order_no",
-                "line",
-                "inventory_id",
-                "gp",
-                "selling_price",
-                "selling_cost",
-                "invoice_no",
-                "customername",
-            ]
-        )
-    )
+    curated_columns = list(set(train_X.columns.to_list()))
 
     # saving the list of relevant columns and the pipeline.
-    save_pipeline(
-        curated_columns, op.abspath(op.join(artifacts_folder, "curated_columns.joblib"))
-    )
-    save_pipeline(
-        features_transformer, op.abspath(op.join(artifacts_folder, "features.joblib"))
-    )
+    save_pipeline(curated_columns, op.abspath(op.join(artifacts_folder, "curated_columns.joblib")))
+    save_pipeline(features_transformer, op.abspath(op.join(artifacts_folder, "features.joblib")))
